125d481dc48e0cabb597fafebfc408ff0be284cb
JosefPeck <--global>
2019-04-24 18:03:49 +0000

    331. Not done at all.

---
+++ challenges/331 [Easy] The Adding Calculator/__main__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.7
+# -*- coding: utf-8 -*-
+"""331 [Easy] The Adding Calculator
+https://old.reddit.com/r/dailyprogrammer/comments/6ze9z0/20170911_challenge_331_easy_the_adding_calculator/
+"""
+
+class Number:
+	def __init__(self):
+		# TODO
+		pass
+
+
+def main():
+	for test in ():
+		pass
